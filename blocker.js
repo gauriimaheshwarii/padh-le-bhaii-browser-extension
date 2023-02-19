51ac6e61505558d1e3e2eeadc9d13655082b2f4b
Gauri Maheshwari <gaurimaheeshwari009@gmail.com>
2023-02-19 20:18:52 +0530

    Add extension files

---
+++ blocker.js
@@ -0,0 +1,30 @@
+const defineCSS = () => {
+  return `<style>
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  img {
+    width: 100%;
+    height: auto;
+  }
+  </style>`
+}
+
+const defineHTML = () => {
+  return `<div class = "content">
+  <img src = "https://media.makeameme.org/created/padh-le-bhai.jpg">
+  </div>`
+}
+
+const forbiddenURLs = ["twitter.com", "web.whatsapp.com"];
+const set = new Set(forbiddenURLs);
+
+if (set.has(window.location.hostname)) {
+  document.head.innerHTML = defineCSS();
+  document.body.innerHTML = defineHTML();
+}
